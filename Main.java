bc623c3ae0c70996d32aa9313fcfc9719b1af720
moislam51 <mohamedislamshaker511@gmail.com>
2026-04-21 22:54:21 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,8 @@
+public class OS_Project {
 
+    public static void main(String[] args) {
+        InputPanel inputWindow = new InputPanel();
+        inputWindow.setVisible(true);        
+        inputWindow.setLocationRelativeTo(null);
+    }
+}
